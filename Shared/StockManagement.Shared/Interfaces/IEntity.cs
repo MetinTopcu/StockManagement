c2d73cd1ad7fff063ccce484b17ba0b204193ad0
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-03-25 01:23:10 +0300

    Shared interfaces have been created

---
+++ Shared/StockManagement.Shared/Interfaces/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement.Shared.Interfaces
+{
+    public interface IEntity<TIDType> where TIDType : struct
+    {
+        TIDType Id { get; set; }
+    }
+}
