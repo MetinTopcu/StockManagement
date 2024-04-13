dc5fdd2ce6526d658913e27bdab045e7e13276eb
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-04-13 17:16:03 +0300

    UnitOfWork has been created

---
+++ Shared/StockManagement.Shared/Domain/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement.Shared.Domain.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork<TDbContext>
+    {
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default(CancellationToken));
+        void BeginTransaction();
+        Task CommitAsync(CancellationToken cancellationToken = default(CancellationToken));
+        void Commit();
+        Task RollbackAsync(CancellationToken cancellationToken = default(CancellationToken));
+        void Rollback();
+    }
+}
---
+++ Shared/StockManagement.Shared/Domain/Services/UnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using StockManagement.Shared.Domain.Interfaces.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockManagement.Shared.Domain.Services
+{
+    public class UnitOfWork<TDbContext> : IUnitOfWork<TDbContext> where TDbContext : DbContext
+    {
+        protected TDbContext DbContext;
+        protected IDbContextTransaction? Transaction { get; private set; } = null;
+
+        public UnitOfWork(TDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public void BeginTransaction()
+        {
+            if (Transaction is null)
+                Transaction = DbContext.Database.BeginTransaction();
+            else throw new InvalidOperationException("A transaction has already been started.");
+        }
+
+        public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+        {
+            if (Transaction is null)
+                Transaction = await DbContext.Database.BeginTransactionAsync(cancellationToken);
+            else throw new InvalidOperationException("A transaction has already been started.");
+
+        }
+
+        public void Commit()
+        {
+            if (Transaction is null)
+                throw new InvalidOperationException("A transaction has not been started.");
+            Transaction.Commit();
+            DbContext.SaveChanges();
+            Transaction.Dispose();
+            Transaction = null;
+        }
+
+        public async Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            if (Transaction is null)
+                throw new InvalidOperationException("A transaction has not been started.");
+            await Transaction.CommitAsync(cancellationToken);
+            await DbContext.SaveChangesAsync(cancellationToken);
+            await Transaction.DisposeAsync();
+            Transaction = null;
+        }
+
+        public void Rollback()
+        {
+            if (Transaction is null)
+                throw new Exception("Transaction is null");
+            Transaction.Rollback();
+            Transaction.Dispose();
+            Transaction = null;
+        }
+
+        public async Task RollbackAsync(CancellationToken cancellationToken = default)
+        {
+            if (Transaction is null)
+                throw new Exception("Transaction is null");
+            await Transaction.RollbackAsync(cancellationToken);
+            await Transaction.DisposeAsync();
+            Transaction = null;
+        }
+    }
+}
