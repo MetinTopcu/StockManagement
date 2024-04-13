b12733c4c5439e76a5735fe93fc7969299981827
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-04-14 00:59:28 +0300

    AutoMapper has been added and Program.cs has been edited

--- Services/Catalog/src/application/StockManagement.Catalog.API/Program.cs
+++ Services/Catalog/src/application/StockManagement.Catalog.API/Program.cs
@@ -1,8 +1,44 @@
+using StockManagement.Catalog.Application.Mapper;
+using StockManagement.Catalog.Application.Service;
+using StockManagement.Catalog.Application.Services;
+using StockManagement.Catalog.ApplicationContracts.ServiceContracts;
+using StockManagement.Catalog.Domain.RepositoryContracts;
+using StockManagement.Catalog.Infrastructure;
+using StockManagement.Catalog.Infrastructure.Repositories;
+using StockManagement.Shared.Domain.Interfaces.Repository;
+using StockManagement.Shared.Domain.Interfaces.UnitOfWork;
+using StockManagement.Shared.Domain.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductItemService, ProductItemService>();
+
+builder.Services.AddScoped<IUnitOfWork<CatalogDbContext>, UnitOfWork<CatalogDbContext>>();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductItemRepository, ProductItemRepository>();
+builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+builder.Services.AddScoped<ICategoryRepository,  CategoryRepository>();
+builder.Services.AddScoped<IUnitRepository, UnitRepository>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(opts =>
+{
+    opts.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:8081", "http://172.16.1.138")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Services/Catalog/src/business/application/StockManagement.Catalog.Application/Mapper/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StockManagement.Catalog.Domain.DTOs;
+using StockManagement.Catalog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement.Catalog.Application.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Product, CreateProductInputDTO>().ReverseMap();
+            CreateMap<ProductItem, CreateProductItemInputDTO>().ReverseMap();
+        }
+    }
+}
