91b307adeacf08d6e3966106b2eaa32fc21295da
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-04-13 18:47:17 +0300

    Catalog DbContext has been created

---
+++ Services/Catalog/src/business/infrastructure/StockManagement.Catalog.Infrastructure/CatalogDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManagement.Catalog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement.Catalog.Infrastructure
+{
+    public class CatalogDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Unit> Units { get; set; }
+
+        public CatalogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
