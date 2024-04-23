d9eb2acd7f04928da25af1082ff70a2f17e7f991
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-04-23 15:39:42 +0300

    Dependencies has been added

--- Services/Authentication/src/application/StockManagement.Authentication.API/Program.cs
+++ Services/Authentication/src/application/StockManagement.Authentication.API/Program.cs
@@ -1,8 +1,36 @@
+using StockManagement.Shared.Domain.Interfaces.UnitOfWork;
+using StockManagement.Shared.Domain.Services;
+using StockManagement.User.Application.Mapper;
+using StockManagement.User.Application.Services;
+using StockManagement.User.ApplicationContracts.ServiceContracts;
+using StockManagement.User.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddScoped<IUnitOfWork<UserDbContext>, UnitOfWork<UserDbContext>>();
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 builder.Services.AddControllers();
+
+builder.Services.AddCors(opts =>
+{
+    opts.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:8081", "http://172.16.1.138")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
