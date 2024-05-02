3027700ea0ffd36719006b6797e2a7da034a45d7
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-05-02 16:06:14 +0300

    Apsetting.json and Program.cs have been updated

--- Services/Authentication/src/application/StockManagement.Authentication.API/Program.cs
+++ Services/Authentication/src/application/StockManagement.Authentication.API/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using StockManagement.Shared.Domain.Interfaces.UnitOfWork;
 using StockManagement.Shared.Domain.Services;
 using StockManagement.User.Application.Mapper;
 using StockManagement.User.Application.Services;
 using StockManagement.User.ApplicationContracts.ServiceContracts;
+using StockManagement.User.Domain.Configuration;
+using StockManagement.User.Domain.Entities;
+using StockManagement.User.Domain.RepositoryContracts;
 using StockManagement.User.Infrastructure;
+using StockManagement.User.Infrastructure.Repositories;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,9 +24,9 @@ builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 
-builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddScoped<IUserRefreshTokenRepository, UserRefreshTokenRepository<UserDbContext>>();
 
-builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 builder.Services.AddCors(opts =>
 {
@@ -30,6 +38,46 @@ builder.Services.AddCors(opts =>
         .AllowCredentials();
     });
 });
+
+builder.Services.AddDbContext<UserDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(UserDbContext))?.GetName().Name);
+    });
+});
+
+builder.Services.AddIdentity<UserApp, IdentityRole>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+    opt.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<UserDbContext>().AddDefaultTokenProviders();
+
+builder.Services.Configure<CustomTokenOptions>(builder.Configuration.GetSection("TokenOptions"));
+builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+{
+    var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<CustomTokenOptions>();
+    opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidIssuer = tokenOptions?.Issuer,
+        ValidAudience = tokenOptions?.Audience?[0],
+        IssuerSigningKey = SignService.GetSymmetricSecuritykey(tokenOptions.SecurityKey),
+
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
