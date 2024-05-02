743f8cce0aa8b5f6508904b6957020d7080048de
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-05-02 16:05:56 +0300

    Controllers have been updated

---
+++ Services/Authentication/src/application/StockManagement.Authentication.API/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StockManagement.User.ApplicationContracts.ServiceContracts;
+using StockManagement.User.Domain.DTOs;
+using StockManagement.User.Domain.Entities;
+
+namespace StockManagement.User.API.Controllers
+{
+    [Route("account")]
+    [Authorize]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<UserAppDTO> Register(CreateUserDTO createUserDto)
+        {
+            var result = await _userService.CreateUserAsync(createUserDto);
+            return result;
+        }
+
+
+        [HttpGet("get-user-by-name")]
+        public async Task<UserAppDTO> GetUserByNameAsync()
+        {
+            //var email = HttpContext.User.Claims.Where(x => x.Type == "email").FirstOrDefault();
+            var result = await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name);
+            return result;
+        }
+
+        [HttpPost("create-user-role")]
+        [AllowAnonymous]
+        public async Task<bool> CreateUserRole(CreateUserRolesDTO createUserRolesDTO)
+        {
+            await _userService.CreateUserRoles(createUserRolesDTO);
+
+            return true;
+        }
+        [HttpPost("create-role")]
+        [AllowAnonymous]
+        public async Task<bool> CreateRole(CreateRolesDTO createRolesDTO)
+        {
+            await _userService.CreateRoles(createRolesDTO);
+            return true;
+        }
+    }
+}
---
+++ Services/Authentication/src/application/StockManagement.Authentication.API/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using StockManagement.User.ApplicationContracts.ServiceContracts;
+using StockManagement.User.Domain.DTOs;
+
+namespace StockManagement.User.API.Controllers
+{
+    [Route("auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost("login")]
+        public async Task<TokenDTO> Login(LoginDTO loginDto)
+        {
+            var result = await _authenticationService.LoginAsync(loginDto);
+
+            return result;
+        }
+
+        [HttpPost("create-token-by-client")]
+        public ClientTokenDTO CreateTokenByClient(ClientLoginDTO clientLoginDto)
+        {
+            var result = _authenticationService.CreateTokenByClient(clientLoginDto);
+
+            return result;
+        }
+
+        [HttpPost("revoke-refresh-token")]
+        public async Task<bool> RevokeRefreshToken(RefreshTokenDTO refreshTokenDto)
+        {
+            await _authenticationService.RevokeRefreshToken(refreshTokenDto.Token);
+
+            return true;
+        }
+
+        [HttpPost("create-token-by-refresh-token")]
+        public async Task<TokenDTO> CreateTokenByRefreshToken(RefreshTokenDTO refreshTokenDto)
+        {
+            var result = await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.Token);
+
+            return result;
+        }
+
+    }
+}
